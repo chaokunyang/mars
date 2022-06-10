cc32e468f986e35c4642db3f53c327b72146ea86
chaokunyang <shawn.ck.yang@gmail.com>
2022-06-10 21:14:50 +0800

    fix kill ray actor

--- mars/oscar/backends/ray/driver.py
+++ mars/oscar/backends/ray/driver.py
@@ -20,7 +20,11 @@ from typing import Dict
 from ....serialization.ray import register_ray_serializers, unregister_ray_serializers
 from ....utils import lazy_import
 from ...driver import BaseActorDriver
-from .utils import process_placement_to_address, addresses_to_placement_group_info
+from .utils import (
+    process_placement_to_address,
+    addresses_to_placement_group_info,
+    kill_and_wait,
+)
 
 ray = lazy_import("ray")
 logger = logging.getLogger(__name__)
@@ -72,15 +76,17 @@ class RayActorDriver(BaseActorDriver):
                     pg_name, index, process_index=process_index
                 )
                 try:
+                    ray_actor = ray.get_actor(address)
                     if "COV_CORE_SOURCE" in os.environ:  # pragma: no cover
                         # must clean up first, or coverage info lost.
                         # must save the local reference until this is fixed:
                         # https://github.com/ray-project/ray/issues/7815
-                        ray_actor = ray.get_actor(address)
                         ray.get(ray_actor.cleanup.remote())
-                    ray.kill(ray.get_actor(address))
+                    logger.info("Kill actor %s", ray_actor)
+                    kill_and_wait(ray_actor, no_restart=True)
                 except:  # noqa: E722  # nosec  # pylint: disable=bare-except
                     pass
+        logger.info("Remove placement_group %s", pg)
         ray.util.remove_placement_group(pg)
         cls._cluster_info = dict()
         unregister_ray_serializers()
